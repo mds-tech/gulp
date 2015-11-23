581158356b597de42c3ea40bc3c0ee6781cbbf67
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2015-11-23 18:14:36 +0200

    Fix config file filename

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var onlyScripts = require('./util/scriptFilter');
 var tasks = fs.readdirSync('./gulp/tasks/').filter(onlyScripts);
 
-global.config = JSON.parse(fs.readFileSync('gulp-paths.json', 'utf8'));
+global.config = JSON.parse(fs.readFileSync('gulp-config.json', 'utf8'));
 
 tasks.forEach(function(task) {
     require('./tasks/' + task);
