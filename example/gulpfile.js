30a6ab883eba96ad6353cae198cb6c41e85b9a31
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2015-11-26 07:55:38 +0200

    Fix paths in main gulp file

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,10 +1,10 @@
 require('./gulp');
 
 var elixir = require('laravel-elixir'),
-    paths  = global.paths;
+    path  = global.config.paths;
 
 elixir(function(mix) {
   mix
     .task('build')
-    .version([paths.dist.css+'style.css', paths.dist.js+'main.min.js', paths.dist.js+'vendor.min.js'])
+    .version([path.dist.css+'style.css', path.dist.js+'main.min.js', path.dist.js+'vendor.min.js'])
 });
