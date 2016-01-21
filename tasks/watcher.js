6bd9e8df0993f4de315e1514c949bb123b394535
Marnu Lombard <marnu@mdstech.co.za>
2016-01-21 10:45:58 +0200

    Pull iconsList task into default task & watcher

--- tasks/watcher.js
+++ tasks/watcher.js
@@ -6,6 +6,8 @@ gulp.task('watcher', ['browserSync'], function() {
   gulp.watch(path.src.js+'**/*', ['custom-scripts']);
   gulp.watch(path.bower+'**/*', ['vendor-scripts']);
   gulp.watch(path.src.sass+'**/*.scss', ['_sass']);
+  gulp.watch(path.src.images+'icons/**/*.svg', ['iconsList']);
+  gulp.watch(path.src.images+'icons/**/*.json', ['iconsSass']);
   gulp.watch(path.src.images+'**/*.svg', ['imagesConvert']);
   gulp.watch(path.src.images+'**/*', ['imagesCompress']);
 });
