ec9993daa5c22595c8084f2c83e424aad9ab40fa
Marnu Lombard <marnu@mdstech.co.za>
2016-10-26 09:52:47 +0200

    Make _scripts search recursively for .json files

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -2,7 +2,7 @@ var gulp   = require('gulp'),
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
     rename = require("gulp-rename"),
-    concat = require('gulp-concat-json2js'),
+    concat = require('gulp-concat-json2js')
     path   = global.config.paths;
 
 gulp.task('scripts', ['_scripts']); // Gets overwritten by Elixir
@@ -10,7 +10,7 @@ gulp.task('_scripts', ['custom-scripts', 'vendor-scripts']);
 
 // Task custom-scripts
 gulp.task('custom-scripts', function() {
-  gulp.src(path.src.js+'**.json')
+  gulp.src(path.src.js+'*/**.json')
     .pipe(concat())
     .pipe(jshint())
     .pipe(jshint.reporter())
@@ -23,7 +23,7 @@ gulp.task('custom-scripts', function() {
 // Task vendor-scripts
 gulp.task('vendor-scripts', function() {
   var src = path.src.vendorJs || path.src.js+'vendor/';
-  gulp.src(src+'**.json')
+  gulp.src(src+'*/**.json')
     .pipe(concat())
     .pipe(uglify())
     .pipe(rename({suffix:'.min'}))
