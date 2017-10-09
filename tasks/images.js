9ce494678ed4783419e75d9d3aaf0f33923e4b7f
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2017-10-09 09:42:45 +0200

    Improve image script

--- tasks/images.js
+++ tasks/images.js
@@ -1,23 +1,21 @@
-var gulp         = require('gulp'),
-    svg2png      = require('gulp-svg2png'),
-    changed      = require('gulp-changed'),
-    imagemin     = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    handleErrors = require('../util/handleErrors'),
-    path         = global.config.paths;
+var gulp = require('gulp'),
+  svg2png = require('gulp-svg2png'),
+  changed = require('gulp-changed'),
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  handleErrors = require('../util/handleErrors'),
+  path = global.config.paths;
 
-gulp.task('images', ['_images']); // Gets overwritten by Elixir
-gulp.task('_images', ['imagesCompress', 'imagesConvert']);
+gulp.task('images', ['imagesCompress', 'imagesConvert']);
 
 gulp.task('imagesConvert', function () {
   return gulp.src(path.src.images + '**/*.svg')
     .pipe(changed(path.dist.images))
     .pipe(svg2png()).on('error', handleErrors)
     .pipe(
-      imagemin({
-        progressive: true,
-        use: [pngquant()]
-      })
+      imagemin([
+        pngquant({speed: 2, quality: 75})
+      ])
       .on('error', handleErrors)
     )
     .pipe(gulp.dest(path.dist.images));
@@ -27,10 +25,20 @@ gulp.task('imagesCompress', function () {
   return gulp.src(path.src.images + '**/*')
     .pipe(changed(path.dist.images))
     .pipe(
-      imagemin({
-        progressive: true,
-        use: [pngquant()]
-      })
+      imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        pngquant({speed: 2, quality: 75}),
+        imagemin.svgo({
+          precision: 0,
+          plugins: [
+            {convertPathData: {floatPrecision: 2}},
+            {cleanupNumericValues: {floatPrecision: 1}},
+            {cleanupListOfValues: {floatPrecision: 1}},
+            {removeViewBox: true}
+          ]
+        })
+      ])
       .on('error', handleErrors)
     )
     .pipe(gulp.dest(path.dist.images))
