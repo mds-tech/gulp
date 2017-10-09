74ee0e8fdf00bfdc580a907d0798784687adda9c
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2017-10-09 09:43:01 +0200

    Cleanup icon script

--- tasks/iconsList.js
+++ tasks/iconsList.js
@@ -1,38 +1,33 @@
-var gulp          = require('gulp'),
-		fs            = require('fs'),
-		toJson        = require('gulp-to-json'),
-		cwd           = require('cwd'),
-		jsonSass      = require('json-sass'),
-		source        = require('vinyl-source-stream'),
-		rename        = require('gulp-rename'),
-		handleErrors  = require('../util/handleErrors'),
-		path          = global.config.paths,
-		iconsVars     = global.config.icons;
+var gulp = require('gulp'),
+  fs = require('fs'),
+  toJson = require('gulp-to-json'),
+  cwd = require('cwd'),
+  jsonSass = require('json-sass'),
+  source = require('vinyl-source-stream'),
+  rename = require('gulp-rename'),
+  path = global.config.paths,
+  iconsVars = global.config.icons;
 
-var basePath  = cwd(path.src.icons),
-		regexPath = new RegExp('('+basePath.replace(/\//g, "\\/")+'\/)|(\.svg)', 'g'),
-		jsonPath  = basePath+'/'+iconsVars.jsonFilePath,
-		sassPath  = path.src.sass+iconsVars.sassFilePath,
-		jsonFile  = iconsVars.jsonFileName,
-		sassFile  = iconsVars.sassFileName;
+var basePath = cwd(path.src.icons),
+  jsonFile = basePath+'/'+iconsVars.jsonFileName;
 
-gulp.task('_icons', ['iconsList', 'iconsSass']);
+gulp.task('icons', ['iconsList', 'iconsSass']);
 
 gulp.task('iconsList', function () {
-	return gulp.src(path.src.icons+'*.svg')
-		.pipe(toJson({
-			filename: jsonPath+jsonFile,
-			strip: regexPath,
-		}));
+  return gulp.src(path.src.icons+'*.svg')
+    .pipe(toJson({
+      filename: jsonFile,
+      strip: new RegExp('('+basePath.replace(/\//g, "\\/")+'\/)|(\.svg)', 'g'),
+    }));
 });
 
 gulp.task('iconsSass', function() {
-	return fs.createReadStream(jsonPath+jsonFile)
-		.pipe(jsonSass({
-			prefix: iconsVars.sassPrefix+': ',
-		}))
-		.pipe(source(jsonPath+jsonFile))
-		.pipe(rename('_'+sassFile))
-		.pipe(gulp.dest(sassPath));
+  return fs.createReadStream(jsonFile)
+    .pipe(jsonSass({
+      prefix: iconsVars.sassPrefix+': ',
+    }))
+    .pipe(source(jsonFile))
+    .pipe(rename('_'+iconsVars.sassFileName))
+    .pipe(gulp.dest(path.src.sass+iconsVars.sassFilePath));
 });
 
