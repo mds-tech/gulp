5b5462df21b0e28db73adfe04c18844f18e5affb
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2017-12-27 15:59:46 +0200

    Check for production on scripts

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -3,29 +3,38 @@ var gulp   = require('gulp'),
     jshint = require('gulp-jshint'),
     rename = require("gulp-rename"),
     concat = require('gulp-concat-json2js'),
+    argv   = require('yargs').argv,
     path   = global.config.paths;
+
+var production = argv.production;
 
 gulp.task('scripts', ['_scripts']); // Gets overwritten by Elixir
 gulp.task('_scripts', ['custom-scripts', 'vendor-scripts']);
 
 // Task custom-scripts
 gulp.task('custom-scripts', function() {
-  gulp.src(path.src.js+'**.json')
+  var stream = gulp.src(path.src.js+'**.json')
     .pipe(concat())
     .pipe(jshint())
     .pipe(jshint.reporter())
-    .pipe(gulp.dest(path.dist.js))
-    .pipe(uglify())
-    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest(path.dist.js));
+  if(production) {
+    stream.pipe(uglify());
+  }
+  stream.pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest(path.dist.js));
 });
 
 // Task vendor-scripts
 gulp.task('vendor-scripts', function() {
   var src = path.src.vendorJs || path.src.js+'vendor/';
-  gulp.src(src+'**.json')
-    .pipe(concat())
-    .pipe(uglify())
-    .pipe(rename({suffix:'.min'}))
+
+  var stream = gulp.src(src+'**.json')
+    .pipe(concat());
+
+  if(production) {
+    stream.pipe(uglify());
+  }
+  stream.pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest(path.dist.js));
 });
